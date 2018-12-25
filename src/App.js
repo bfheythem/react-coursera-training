97430809110ff64235ee3259aef2572e782fd912
Heythem Ben Fadhel <HBenFadhel@se.linedata.com>
2018-12-25 11:25:27 +0100

    Assignment 1

--- src/App.js
+++ src/App.js
@@ -25,33 +25,4 @@ class App extends Component {
     );
   }
 }
-/* class App extends Component {
-  render() {
-    return (
-      <div className="App">
-     <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        
-      </div>
-    );
-  }
-} */
-
 export default App;
